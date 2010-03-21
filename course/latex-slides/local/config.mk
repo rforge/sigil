35c54094c897cdbf291f5faacd91d4dad4367cd5
schtepf <schtepf@edb9625f-4e0d-4859-8d74-9fd3b1da38cb>
2010-03-21 19:24:47 +0000

    Reorganisation of latex slides
    git-svn-id: svn://svn.r-forge.r-project.org/svnroot/sigil@4 edb9625f-4e0d-4859-8d74-9fd3b1da38cb

---
+++ course/latex-slides/local/config.mk
@@ -0,0 +1,10 @@
+##
+##  local configuration of Makefile for LaTeX-Beamer slides
+##
+
+## specify names of presentations here (with extension ".pdf")
+FILES := 01_introduction.pdf 04a_collocations_1.pdf 04b_collocations_2.pdf 05_zipfr.pdf 06_regression_linear_models.pdf 07_exploratory_data_analysis.pdf 09_iaa.pdf
+
+## directory where generated PDF documents can be installed
+INSTALL_DIR := ../pdf
+
