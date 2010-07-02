e363bd09018a2019b64221568b823fc462378eee
schtepf <schtepf@edb9625f-4e0d-4859-8d74-9fd3b1da38cb>
2010-07-02 10:05:45 +0000

    Updated version of Unit 2 with minor improvements
    git-svn-id: svn://svn.r-forge.r-project.org/svnroot/sigil@23 edb9625f-4e0d-4859-8d74-9fd3b1da38cb

--- course/latex-slides/local/config.mk
+++ course/latex-slides/local/config.mk
@@ -3,7 +3,7 @@
 ##
 
 ## specify names of presentations here (with extension ".pdf")
-FILES := 01_introduction.pdf 04a_collocations_1.pdf 04b_collocations_2.pdf 05_zipfr.pdf 06_regression_linear_models.pdf 07_exploratory_data_analysis.pdf 09_iaa.pdf
+FILES := 01_introduction.pdf 03a_continuous_descriptive.pdf 03b_continuous_inferential.pdf 04a_collocations_1.pdf 04b_collocations_2.pdf 05_zipfr.pdf 06_regression_linear_models.pdf 07_exploratory_data_analysis.pdf 09_iaa.pdf
 
 ## directory where generated PDF documents can be installed
 INSTALL_DIR := ../pdf
