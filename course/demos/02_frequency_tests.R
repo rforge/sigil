1a87efafc37d25b4a0de1965a0a978e13407d935
schtepf <schtepf@edb9625f-4e0d-4859-8d74-9fd3b1da38cb>
2014-05-25 15:37:42 +0000

    Unit #2 revisions complete
    git-svn-id: svn://svn.r-forge.r-project.org/svnroot/sigil@46 edb9625f-4e0d-4859-8d74-9fd3b1da38cb

--- course/demos/02_frequency_tests.R
+++ course/demos/02_frequency_tests.R
@@ -2,7 +2,8 @@
 ## Unit 2: Corpus frequency data & statistical inference
 ## -- code examples --
 
-# relevant information from output of binom.test() is the p-value
+
+## relevant information from output of binom.test() is the p-value
 binom.test(19, 100, p=.15) # two-sided
 binom.test(19, 100, p=.15, alternative="greater") # one-sided
 
@@ -10,14 +11,14 @@ binom.test(19, 100, p=.15)$p.value
 binom.test(23, 100, p=.15)$p.value
 binom.test(190, 1000, p=.15)$p.value
 
-# focus on confidence interval now (no null hypothesis specified)
+## focus on confidence interval now (no null hypothesis specified)
 binom.test(190, 1000, conf.level=.99)
 
 library(corpora) # package 'corpora' can be installed from CRAN
 prop.cint(190, 1000, conf.level=.99)
 ?prop.cint # "conf. intervals for proportions"
 
-# plot confidence interval curves (e.g. for choosing sample size)
+## plot confidence interval curves (e.g. for choosing sample size)
 plot(0:1, 0:1, type="n", xaxs="i", yaxs="i", xlab="sample proportion p", ylab=expression("population  value " * pi), main="99% confidence intervals")
 abline(h=seq(0,1,.1), v=seq(0,1,.1), col="grey") # background grid
 abline(0, 1, lwd=2, col="black") # maximum-likelihood estimate = MLE
@@ -33,7 +34,7 @@ lines(k/n, conf$lower, lwd=2, col="red")
 lines(k/n, conf$upper, lwd=2, col="red")
 legend("topleft", inset=.05, bg="white", lwd=3, col=c("black","red","blue"), legend=c("MLE", "n - 200", "n = 50"))
 
-# frequency comparison: proportions test, chi-squared, Fisher's test
+## frequency comparison: proportions test, chi-squared, Fisher's test
 prop.test(c(19,25), c(100,200)) # notice conf. int. for difference of proportions
 
 ct <- cbind(c(19,81), c(25,175)) # construct contingency table by columns
@@ -43,11 +44,17 @@ fisher.test(ct) # Fisher's exact test (conf. int. for odds ratio)
 
 ct <- cont.table(19, 100, 25, 200) # lazy solution from 'corpora' library
 
-## Now, perform frequency comparison for number of passives in Brown (AmE)
+
+## Case study: frequency comparison for number of passives in Brown (AmE)
 ## and LOB (BrE) corpora (both pooled data and for each section / genre).
 
 Brown <- read.csv("passives.brown.csv") # load CSV data files
 LOB <- read.csv("passives.lob.csv")
+
+## alternatively, data sets are also included in the SIGIL package
+library(SIGIL)
+Brown <- BrownPassives
+LOB <- LOBPassives
 
 Brown # table format should be self-explanatory
 LOB
@@ -58,17 +65,17 @@ LOB.all <- colSums(LOB[, 2:4])
 Brown.all # now copy relevant data into contingency table
 LOB.all
 
-ct <- cbind(c(10123, 49576-10123), c(10934, 49742-10934)) # Brown, LOB
-ct
-fisher.test(ct)
-
 prop.test(c(10123, 10934), c(49576, 49742)) # effect size = difference of proportions
 
-# we can do the same (manually) for every genre in the Brown / LOB corpus;
-# convenient: write our own function that shows relevant information concisely
+ct <- cbind(c(10123, 49576-10123), c(10934, 49742-10934)) # Brown, LOB
+ct
+fisher.test(ct) # effect size = odds ratio
+
+## we can do the same (manually) for every genre in the Brown / LOB corpus;
+## convenient: write our own function that shows relevant information concisely
 do.test <- function (k1, n1, k2, n2) {
 	res <- prop.test(c(k1, k2), c(n1, n2)) # result of proportions test
-	# data frames are a nice way to display summary tables
+	## data frames are a nice way to display summary tables
 	fmt <- data.frame(p=res$p.value, lower=res$conf.int[1], upper=res$conf.int[2])
 	fmt # return value of function = last expression
 }
@@ -76,46 +83,46 @@ do.test <- function (k1, n1, k2, n2) {
 do.test(10123, 49576, 10934, 49742) # for the pooled data again
 do.test(146, 975, 134, 947) # humour genre (category R)
 
-# we can also extract these values directly from the data frames
+## we can also extract these values directly from the data frames
 do.test(Brown$passive[15], Brown$n_s[15], LOB$passive[15], LOB$n_s[15])
 
-# nicer version of user function with genre category labels do.test <- function (k1, n1, k2, n2, cat="") {
-	res <- prop.test(c(k1, k2), c(n1, n2))
-	data.frame(
-		p=res$p.value,
-		lower=100*res$conf.int[1], # scaled to % points
-		upper=100*res$conf.int[2],
-		row.names=cat   # add genre as row label
-	) # return data frame directly without local variable fmt
+## nicer version of user function with genre category labels 
+do.test <- function (k1, n1, k2, n2, cat="") {
+  res <- prop.test(c(k1, k2), c(n1, n2))
+  data.frame(
+    p=res$p.value,
+    lower=100*res$conf.int[1], # scaled to % points
+    upper=100*res$conf.int[2],
+    row.names=cat   # add genre as row label
+    ) # return data frame directly without local variable fmt
 }
 
-do.test(	Brown$passive[15], Brown$n_s[15],
-			LOB$passive[15], LOB$n_s[15], cat=Brown$name[15])
+do.test(Brown$passive[15], Brown$n_s[15], LOB$passive[15], LOB$n_s[15], cat=Brown$name[15])
 
-# ad-hoc convenience function to reduce typing/editing
-# (works only if global Brown/LOB variables are set correctly!)
+## ad-hoc convenience function to reduce typing/editing
+## (works only if global Brown/LOB variables are set correctly!)
 quick.test <- function (i) {
-		do.test(	k1=Brown$passive[i], n1=Brown$n_s[i],
-					k2=LOB$passive[i], n2=LOB$n_s[i],
-					cat=Brown$name[i] )
+  do.test(k1=Brown$passive[i], n1=Brown$n_s[i],
+          k2=LOB$passive[i], n2=LOB$n_s[i],
+          cat=Brown$name[i] )
 }
 
 quick.test(15)
 quick.test(9)
 
-# loop over all 15 rows (or use 1:nrow(Brown) to count rows automatically)
+## loop over all 15 rows (or use 1:nrow(Brown) to count rows automatically)
 for (i in 1:15) {
-	print( quick.test(i) )
+  print( quick.test(i) )
 }
 
-# our code relies on same ordering of genres in Brown and LOB
+## our code relies on same ordering of genres in Brown and LOB
 all(Brown$cat == LOB$cat) 
 
-# we need some R wizardry to collect the 15 genre results into a single table
-#  - lapply() applies a function to each element of a list or vector
-#  - returns list with result values (here: table rows)
-#  - use rbind() to combine rows into single table
-#  - rows have to be passed as individual arguments using do.call()
+## we need some R wizardry to collect the 15 genre results into a single table
+##  - lapply() applies a function to each element of a list or vector
+##  - returns list with result values (here: table rows)
+##  - use rbind() to combine rows into single table
+##  - rows have to be passed as individual arguments using do.call()
 res.list <- lapply(1:15, quick.test)
 
 res <- do.call(rbind, res.list) # as if each list element were an argument
@@ -123,8 +130,8 @@ res <- do.call(rbind, res.list) # as if each list element were an argument
 res
 round(res, 3) # easier to read after rounding
 
-# Which differences are significant? Are the effect sizes linguistically relevant?
+## Which differences are significant? Are the effect sizes linguistically relevant?
 
-# Use cont.table() from corpora package to create list of contingency tables,
-# then lapply() or sapply() Fisher's test and chi-squared directly to each table.
+## Use cont.table() from corpora package to create list of contingency tables,
+## then lapply() or sapply() Fisher's test and chi-squared directly to each table.
 
--- course/illustrations/R/02_sampling_distribution.R
+++ course/illustrations/R/02_sampling_distribution.R
@@ -260,7 +260,7 @@ draw.ppm <- function (K, col="black", lty="solid", lwd=3) {
   lines(p, 1e6 * cint$lower, lwd=lwd, col=col, lty=lty)
 }
 
-screen.device(width=8, height=8)
+screen.device(width=8, height=8, bg="white")
 par(cex=1.4, mar=c(4,4,1,1)+.1, xaxs="i", yaxs="i")
 
 plot(c(0,100), c(0,100), type="l", lwd=3, xlab="sample: k / n (%)", ylab=expression("population: " * pi * " (%)"), main="")
@@ -300,74 +300,3 @@ legend("topright", inset=.05, bg="white",
        legend=c("MLE", "n = 100M", "n = 10M", "n = 5M", "n = 1M", "n = 500k"),
        lwd=4, col=c("black", "orange", "darkgreen", "red", "blue", "grey"))
 dev.copy2pdf(file="../keynote-slides/img/sample_size_3.pdf", onefile=FALSE, bg="white")
-
-
-
-
-
-##
-## 2) Greatly increased sampling variation if data from larger sampling units are pooled
-##    (here: two books from sections of library with pi=10% and pi=40%, vs. random sample with pi=25%)
-k <- 0:50
-n1 <- 50
-n2 <- 50
-n <- n1 + n2
-p1 <- .1
-p2 <- .4
-p <- (n1 * p1 + n2 * p2) / n
-
-d.random <- dbinom(k, n, p) # binomial sampling distribution for random sample of size n
-tail.random <- sapply(k, function (k) binom.test(k, n, p)$p.value < .05)
-## for pooled data, we have sample of 100 items from section 1 in 25% of cases, sample of 100 items from section 2 in another 25% of cases
-d.p11 <- .25 * dbinom(k, n, p1)
-d.p22 <- .25 * dbinom(k, n, p2)
-## in the remaining 50% of cases, we pool a sample of 50 items from section 1 and a sample of 50 items from section 2
-## (this is close to a random sample of 100 items from the full library, but we derive the precise distribution by explicit summation)
-k.grid <- expand.grid(k1=0:n1, k2=0:n2)
-k.grid <- transform(k.grid, d1=dbinom(k1, n1, p1), d2=dbinom(k2, n2, p2))
-k.grid <- transform(k.grid, d=d1*d2, k=k1+k2)
-d.p12 <- .50 * as.vector(rowsum(k.grid$d, factor(k.grid$k, levels=0:n)))
-d.pooled <- d.p11 + d.p12[seq(along=k)] + d.p22
-
-screen.device(width=12, height=5, bg="white")
-par(cex=1.5, mar=c(4,4,0,0)+.5, xaxs="i", yaxs="i");
-
-barplot(100 * d.random, names.arg=ifelse(k %% 5 == 0, k, NA), ylim=c(0,10),
-        col=ifelse(tail.random, "black", "grey"), density=ifelse(tail.random, 10, NA), angle=45, 
-        xlab="value k of observed frequency X", ylab="percentage of samples with X=k")
-dev.copy2pdf(file="../keynote-slides/img/pooled_data_100_tokens.pdf", onefile=FALSE)
-
-barplot(100 * d.pooled, names.arg=ifelse(k %% 5 == 0, k, NA), ylim=c(0,10), col="grey",
-        xlab="value k of observed frequency X", ylab="percentage of samples with X=k")
-dev.copy2pdf(file="../keynote-slides/img/pooled_data_2_books.pdf", onefile=FALSE)
-
-dev.off()
-
-
-## 3) Comparing binomial and Gaussian sampling distributions
-##    (to illustrate that Gaussian distribution can model overdispersion by controlling spread of the distribution)
-compare.binomial.gaussian <- function (p, n=100, sigma=sqrt(n*p*(1-p)), k.range=0:n, y.max=NULL) {
-  mu <- n * p
-  d.binom <- 100 * dbinom(k.range, n, p)
-  d.gauss <- 100 * dnorm(k.range, mu, sigma)
-  if (missing(y.max)) y.max <- max(d.binom, d.gauss)
-  plot(range(k.range), c(0, y.max), type="n", xaxs="i", yaxs="i", xlab="", ylab="")
-  lines(k.range, d.binom, lwd=3, col="blue")
-  lines(k.range, d.gauss, lwd=3, col="red")
-  legend.txt <- eval(bquote(expression(B(.(n),.(p)), N(.(mu),.(round(sigma,2))))))
-  legend("topright", inset=.02, legend=legend.txt, lwd=4, col=c("blue", "red"))
-}
-
-screen.device(width=12, height=8, bg="white")
-par(mar=c(2,2,2,2)+.5, mfrow=c(2,3), cex=.8)
-
-compare.binomial.gaussian(.2, sigma=2.5, k.range=0:40, y.max=20)
-compare.binomial.gaussian(.2, k.range=0:40, y.max=20)
-compare.binomial.gaussian(.2, sigma=8, k.range=0:40, y.max=20)
-
-compare.binomial.gaussian(.1, sigma=2, k.range=0:40, y.max=20)
-compare.binomial.gaussian(.1, k.range=0:40, y.max=20)
-compare.binomial.gaussian(.1, sigma=6, k.range=0:40, y.max=20)
-
-dev.copy2pdf(file="../keynote-slides/img/binomial_vs_gaussian.pdf", onefile=FALSE)
-dev.off()
