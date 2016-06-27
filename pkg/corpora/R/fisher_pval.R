8cae6acacb2bd563d4715ec9541f28260f91bc07
schtepf <schtepf@edb9625f-4e0d-4859-8d74-9fd3b1da38cb>
2016-06-27 06:35:29 +0000

    corpora pkg: fisher.pval() now accepts log.p=TRUE
    git-svn-id: svn://svn.r-forge.r-project.org/svnroot/sigil@91 edb9625f-4e0d-4859-8d74-9fd3b1da38cb

--- pkg/corpora/R/fisher_pval.R
+++ pkg/corpora/R/fisher_pval.R
@@ -1,5 +1,5 @@
 fisher.pval <- function (k1, n1, k2, n2,
-                        alternative=c("two.sided", "less", "greater")) {
+                        alternative=c("two.sided", "less", "greater"), log.p=FALSE) {
   alternative <- match.arg(alternative)
   
   if (any(k1 < 0) || any(k1 > n1) || any(n1 <= 0)) stop("k1 and n1 must be integers with 0 <= k1 <= n1")
@@ -14,10 +14,18 @@ fisher.pval <- function (k1, n1, k2, n2,
 
   k <- k1 + k2
 
-  pval <- switch(alternative,
-                 greater   = phyper(k1 - 1, n1, n2, k, lower.tail=FALSE),
-                 less      = phyper(k1, n1, n2, k, lower.tail=TRUE),
-                 two.sided = 2 * pmin(phyper(k1 - 1, n1, n2, k, lower.tail=FALSE), phyper(k1, n1, n2, k, lower.tail=TRUE)))
-  pval <- pmax(0, pmin(1, pval))        # clamp p-value to range [0,1] (may be > 1 in two-sided approximation)
+  if (alternative == "two.sided") {
+    if (log.p) {
+      pval <- pmin(phyper(k1 - 1, n1, n2, k, lower.tail=FALSE, log.p=TRUE), phyper(k1, n1, n2, k, lower.tail=TRUE, log.p=TRUE)) + log(2)
+      pval <- pmin(pval, 0) # clamp p-value to range [0,1] (may be > 1 in two-sided approximation)
+    } else {
+      pval <- 2 * pmin(phyper(k1 - 1, n1, n2, k, lower.tail=FALSE), phyper(k1, n1, n2, k, lower.tail=TRUE))
+      pval <- pmax(0, pmin(1, pval)) # clamp p-value to range [0,1] (may be > 1 in two-sided approximation)
+    }
+  } else if (alternative == "greater") {
+    pval <- phyper(k1 - 1, n1, n2, k, lower.tail=FALSE, log.p=log.p)
+  } else if (alternative == "less") {
+    pval <- phyper(k1, n1, n2, k, lower.tail=TRUE, log.p=log.p)
+  }
   pval
 }
